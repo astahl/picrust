62e74bd45d5a7b82840fd1c4fa83b61222c7b137
Andreas Stahl <andreasbjornstahl@gmail.com>
2023-11-27 19:54:37 +0100

    cleanr use directives? maybe?

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,6 +1,4 @@
 
-use crate::peripherals::mailbox;
-
 pub struct Framebuffer {
     ptr: *mut u8,
     pub size_bytes: usize,
@@ -12,8 +10,8 @@ pub struct Framebuffer {
 
 impl Framebuffer {
     pub fn new() -> Option<Self> {
-        use mailbox::*;
-        let mut mailbox = mailbox::Mailbox::<256>::new();
+        use crate::peripherals::mailbox::*;
+        let mut mailbox = Mailbox::<256>::new();
         let mut result = Framebuffer{
             ptr: 0 as *mut u8,
             size_bytes: 0,
@@ -22,22 +20,23 @@ impl Framebuffer {
             bits_per_pixel: 0,
             pitch_bytes: 0,
         };
+        use PropertyMessageRequest::*;
         let messages = [
-            PropertyMessageRequest::FbSetPhysicalDimensions{width_px: 1280, height_px: 720},
-            PropertyMessageRequest::FbSetVirtualDimensions{width_px: 1280, height_px: 720},
-            PropertyMessageRequest::FbSetDepth{bpp: 32},
-            PropertyMessageRequest::Null
+            FbSetPhysicalDimensions{width_px: 1280, height_px: 720},
+            FbSetVirtualDimensions{width_px: 1280, height_px: 720},
+            FbSetDepth{bpp: 32},
+            Null
         ];
     
         if let Ok(response) = mailbox.request(8, &messages) 
         {
-            use PropertyMessageResponse;
+            use PropertyMessageResponse::*;
             match response {
                 [
-                    PropertyMessageResponse::FbSetPhysicalDimensions{width_px: ph_w, height_px: ph_h},
-                    PropertyMessageResponse::FbSetVirtualDimensions{width_px: v_w, height_px: v_h},
-                    PropertyMessageResponse::FbSetDepth{bpp},
-                    PropertyMessageResponse::Null
+                    FbSetPhysicalDimensions{width_px: ph_w, height_px: ph_h},
+                    FbSetVirtualDimensions{width_px: v_w, height_px: v_h},
+                    FbSetDepth{bpp},
+                    Null
                 ] => {
                     result.width_px = v_w as usize;
                     result.height_px = v_h as usize;
@@ -52,24 +51,24 @@ impl Framebuffer {
         }
     
         let messages: [PropertyMessageRequest; 3] = [
-            PropertyMessageRequest::FbAllocateBuffer {
+            FbAllocateBuffer {
                 alignment_bytes: 16
             },
-            PropertyMessageRequest::FbGetPitch,
-            PropertyMessageRequest::Null
+            FbGetPitch,
+            Null
         ];
     
         if let Ok(response) = mailbox.request(8, &messages) 
         {
-            use PropertyMessageResponse;
+            use PropertyMessageResponse::*;
             match response {
                 [
-                    PropertyMessageResponse::FbAllocateBuffer{
+                    FbAllocateBuffer{
                         base_address_bytes,
                         size_bytes,
                     },
-                    PropertyMessageResponse::FbGetPitch{bytes_per_line},
-                    PropertyMessageResponse::Null
+                    FbGetPitch{bytes_per_line},
+                    Null
                 ] => {
                     result.ptr = base_address_bytes as *mut u8;
                     result.size_bytes = size_bytes as usize;
@@ -86,8 +85,16 @@ impl Framebuffer {
         Some(result)
     }
 
-    pub fn set_pixel(&self, x: usize, y: usize, value: u32) {
+    pub fn set_pixel_a8r8g8b8(&self, x: usize, y: usize, value: u32) {
         unsafe { *self.ptr.add(self.pitch_bytes * y + x*4).cast::<u32>() = value; }
+    }
+
+    pub fn as_pixels(&self) -> &[u32] {
+        unsafe { core::slice::from_raw_parts(self.ptr.cast::<u32>(), self.size_bytes / 4) }
+    }
+
+    pub fn as_mut_pixels(&self) -> &[u32] {
+        unsafe { core::slice::from_raw_parts_mut(self.ptr.cast::<u32>(), self.size_bytes / 4) }
     }
 
     pub fn as_slice(&self) -> &[u8] {
